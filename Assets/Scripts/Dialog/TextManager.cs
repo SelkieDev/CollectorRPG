35d97783436ded4283eee8d77dc4e06b53c603d6
Taylor Moore <tjosephmoore@gmail.com>
2016-03-06 04:09:32 -0600

    Text box

---
+++ Assets/Scripts/Dialog/TextManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextManager : MonoBehaviour {
+
+	public GameObject textBox;
+    public Transform canvas;
+
+    public RectTransform spawn;
+
+    public void SpawnTextBox(RectTransform location, string text, float dialogSpeed) {
+        GameObject newTextBox = Instantiate(textBox, location.anchoredPosition, location.rotation) as GameObject;
+        newTextBox.GetComponent<TextBox>().TypeText(text, dialogSpeed);
+        newTextBox.transform.SetParent(canvas, false);
+    }
+
+    void Start() {
+    	SpawnTextBox(spawn, "What the fuck? What the fuck? What the fuck? What the fuck? What the fuck? What the fuck?", .04f);
+    }
+}
