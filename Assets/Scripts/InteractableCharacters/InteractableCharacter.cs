4e44d89c0885a99c87d09f1eb6bee725e49bd0a6
Taylor Moore <tjosephmoore@gmail.com>
2016-03-07 21:58:11 -0600

    Added some new placeholder art that is a lot less ugly for us to dev on. Added art for a monster (for battle prototyping). Added palceholder music and sound effects for us to prototype how our sound will work (currently have some UI blips, selects and deselects, a "hurt" noise for when monster takes damage, and a "bump" noise for when you collide with something in the overworld.

--- Assets/Scripts/InteractableCharacters/InteractableCharacter.cs
+++ Assets/Scripts/InteractableCharacters/InteractableCharacter.cs
@@ -10,7 +10,7 @@ public class InteractableCharacter : MonoBehaviour {
 	}
 
 	public void Interaction() {
-		//TextManager.SpawnTextBox("<color=cyan>I'm</color> <color=purple>a</color> <color=red>rock</color> <size=150>OMG this works!</size> Hahaha I can't believe it!");
+		TextManager.SpawnTextBox("<color=cyan>I'm</color> <color=purple>a</color> <color=red>rock</color> <size=150>OMG this works!</size> Hahaha I can't believe it!");
 		if (spriteRenderer.color == Color.red) {
 			spriteRenderer.color = Color.blue;
 		} else {
