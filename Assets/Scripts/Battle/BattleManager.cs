af3a2fa8703ff953d63e7b40f2a6a50690a8e288
Taylor Moore <tjosephmoore@gmail.com>
2016-03-11 05:00:22 -0600

    Began work in progress code for status manager class for our demons.

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -4,16 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class BattleManager : MonoBehaviour {
 
-	public Transform friendlySpawnHelper, enemySpawnHelper;
-	static Transform friendlySpawn, enemySpawn;
-
 	static GameObject[] playerParty;
 	static GameObject[] enemyParty;
-
-	void Start() {
-		friendlySpawn = friendlySpawnHelper;
-		enemySpawn = enemySpawnHelper;
-	}
 
 	/** Going to use resources.load to get prefabs and then set their appropriate values. */
 
@@ -23,19 +15,6 @@ public class BattleManager : MonoBehaviour {
 
 	public static void SetEnemyParty(GameObject[] enemysParty) {
 		enemyParty = enemysParty;
-	}
-
-	void OnLevelWasLoaded(int level) {
-		if (level == 1) {
-			friendlySpawn = friendlySpawnHelper;
-			enemySpawn = enemySpawnHelper;
-			foreach (GameObject member in playerParty) {
-				Instantiate(member, friendlySpawn.position, friendlySpawn.rotation);
-			}
-			foreach (GameObject member in enemyParty) {
-				Instantiate(member, friendlySpawn.position, friendlySpawn.rotation);
-			}
-		}
 	}
 
 	public static void StartBattle() {
--- Assets/Scripts/Demons/DemonStatusManager.cs
+++ Assets/Scripts/Demons/DemonStatusManager.cs
@@ -3,6 +3,54 @@ using System.Collections;
 
 public class DemonStatusManager : MonoBehaviour {
 
-	int health, speed, damage, element;
-	string name;
+	[SerializeField]
+	int seed, health, level, speed, damage, currentExperience, experienceToNextLevel;
+	[SerializeField]
+	string element;
+	//Add modifier values to be temporarily buffed and debuffed for health, speed, damage etc.
+	//Add boolean values to track buffs and debuffs.
+
+	void Start() {
+		//If no seed exists in file.
+		seed = Random.Range(1, 5000);
+	}
+
+	public void SetLevel(int newLevel) {
+		level = newLevel;
+		//Increase all stats to reflect new the level by incrementing by a flat value based upon the seed determined on creation.
+	}
+
+	public void ModifyHealth(int value, string ele) {
+		//Value of health modification will be further modified by the elemental type being passed in.
+		health += value;
+
+		if(health <= 0) {
+			Death();
+		}
+	}
+
+	public void CalculateBaseStats(int seed) {
+		//Determine our base stats with seed to be serialized and stored.
+	}
+
+	public void AddBuff() {
+		//TO DO.
+	}
+
+	public void RemoveBuff() {
+		//TO DO
+	}
+
+	public void Death() {
+		Debug.Log(gameObject.name + "has died.");
+	}
+
+	public void AddExperience(int xp) {
+		currentExperience += xp;
+
+		while(currentExperience >= experienceToNextLevel) {
+			currentExperience = currentExperience - experienceToNextLevel;
+			SetLevel(level + 1);
+		}
+	}
 }
